ac9e1103f329ea28b6ac077263180042611ed585
unknown <you@example.com>
2022-04-24 22:57:30 +0200

    second commit

--- src/test/java/com/Sportifbackend/EcommercebackendApplicationTests.java
+++ src/test/java/com/Sportifbackend/EcommercebackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.michaelakamihe.ecommercebackend;
+package com.Sportifbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommercebackendApplicationTests {
+class SportifbackendApplicationTests {
 
 	@Test
 	void contextLoads() {
